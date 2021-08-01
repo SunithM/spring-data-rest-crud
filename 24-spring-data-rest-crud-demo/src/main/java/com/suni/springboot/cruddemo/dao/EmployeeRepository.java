cf9d385dec34e797ac5c4ca94a29200c0009e5bc
SunithM <sunithsuni09@gmail.com>
2021-08-01 13:36:33 +0530

    First commit

---
+++ 24-spring-data-rest-crud-demo/src/main/java/com/suni/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.suni.springboot.cruddemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.suni.springboot.cruddemo.entity.Employee;
+
+//@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+//no need to write implemantation class
+}
---
+++ 24-spring-data-rest-crud-demo/src/test/java/com/suni/springboot/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suni.springboot.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
